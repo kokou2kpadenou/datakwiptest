2125ccba6491ea14f96bd8babb96a969dc6c1f3f
kokou kpadenou <kkokou@gmail.com>
2020-03-01 03:00:08 -0500

    removed jquery

--- src/index.js
+++ src/index.js
@@ -5,7 +5,6 @@ import App from "./components/app/App";
 import * as serviceWorker from "./serviceWorker";
 import "bootstrap/dist/css/bootstrap.min.css";
 import "bootstrap/dist/js/bootstrap.bundle";
-import "jquery";
 
 ReactDOM.render(<App />, document.getElementById("root"));
 
