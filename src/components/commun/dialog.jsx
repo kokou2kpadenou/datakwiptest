f8d226c804c889db2b6d293bfe8c321c25b91454
kokou kpadenou <kkokou@gmail.com>
2020-02-29 20:35:21 -0500

    dialog component

---
+++ src/components/commun/dialog.jsx
@@ -0,0 +1,33 @@
+import React from "react";
+import PropTypes from "prop-types";
+
+const Dialog = ({ title, id, children }) => {
+  return (
+    <div className="modal" id={`${id}-dlg`} tabIndex="-1" role="dialog">
+      <div className="modal-dialog" role="document">
+        <div className="modal-content">
+          <div className="modal-header">
+            <h5 className="modal-title">{title}</h5>
+            <button
+              type="button"
+              className="close"
+              data-dismiss="modal"
+              aria-label="Close"
+            >
+              <span aria-hidden="true">&times;</span>
+            </button>
+          </div>
+          {children}
+        </div>
+      </div>
+    </div>
+  );
+};
+
+Dialog.propTypes = {
+  title: PropTypes.string.isRequired,
+  id: PropTypes.any.isRequired,
+  children: PropTypes.element.isRequired
+};
+
+export default Dialog;
