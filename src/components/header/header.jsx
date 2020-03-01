7861e81024654180fb6f6b2cf9f7e1d0ce553078
kokou kpadenou <kkokou@gmail.com>
2020-02-29 20:37:18 -0500

    header component

---
+++ src/components/header/header.jsx
@@ -0,0 +1,20 @@
+import React from "react";
+import logo from "./Datakwip_logo.png";
+
+const Header = () => {
+  return (
+    <header className="bg-light p-4 mb-4">
+      <h1 className="d-flex align-items-center">
+        <img
+          className="mr-4"
+          style={{ width: "80px", height: "70px" }}
+          src={logo}
+          alt="Datakwip logo"
+        />
+        <span>Datakwip Frontend Test</span>
+      </h1>
+    </header>
+  );
+};
+
+export default Header;
