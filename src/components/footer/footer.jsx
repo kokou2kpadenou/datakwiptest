0c8a4d83a289ececd879b6e2128167e6096efc74
kokou kpadenou <kkokou@gmail.com>
2020-02-29 20:35:56 -0500

    footer component

---
+++ src/components/footer/footer.jsx
@@ -0,0 +1,17 @@
+import React from "react";
+
+const Footer = () => {
+  return (
+    <footer className="text-center py-5" style={{ fontSize: "0.8rem" }}>
+      <div>© 2020 Datakwip. All rights reserved.</div>
+      <div>
+        Developer by Kokou Kpadenou{" "}
+        <span role="img" aria-label="Emoji">
+          &#128526;
+        </span>
+      </div>
+    </footer>
+  );
+};
+
+export default Footer;
