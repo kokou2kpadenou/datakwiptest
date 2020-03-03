2e17d3218c7c56319a478f6b463486010453cabc
kokou kpadenou <kkokou@gmail.com>
2020-03-03 13:20:43 -0500

    level up warminigMsg component to main

--- src/components/main/main.jsx
+++ src/components/main/main.jsx
@@ -1,26 +1,23 @@
 import React from "react";
 import "bootstrap/dist/js/bootstrap.bundle";
-import Data from "./data/data";
-import Select from "./select";
-import Charts from "./charts/charts";
+
+import Setting from "./settings";
+import Graphics from "./graphics";
+import WarningMsg from "./warningMsg";
 
 const Main = ({ state, dispatch }) => {
   return (
     <main className="container">
+      {/* Warning Dialog Box */}
+      <WarningMsg data={state.warning} />
+
       <div className="row">
         <section className="col-12 col-lg-6 p-3">
-          <Select
-            templates={state.templates}
-            currentTemplate={state.currentTemplate}
-            dispatch={dispatch}
-          />
-          <Data state={state} dispatch={dispatch} />
+          <Setting state={state} dispatch={dispatch} />
         </section>
+
         <section className="col-12 col-lg-6 p-3">
-          <Charts
-            elements={state.elements}
-            currentTemplate={state.currentTemplate}
-          />
+          <Graphics graphics={state.graphics} dispatch={dispatch} />
         </section>
       </div>
     </main>
